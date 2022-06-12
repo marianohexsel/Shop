e4fdb7562f9951fa2c1c59831adfdf343f9aa37f
Mariano Hexsel <marianohexsel@gmail.com>
2022-06-12 14:41:09 -0300

    Arrow functions to reduce code lines.

--- Shop.Domain/Entities/ShopList.cs
+++ Shop.Domain/Entities/ShopList.cs
@@ -5,15 +5,11 @@ namespace Shop.Domain.Entities;
 
 public class ShopList : Entity
 {
-    public ShopList(CreateShopListCommand command)
-    {
+    public ShopList(CreateShopListCommand command) =>
         UpdateTitle(command.Title);
-    }
 
     public string Title { get; private set; } = string.Empty;
     
-    public void UpdateTitle(string title)
-    {
+    public void UpdateTitle(string title) =>
         Title = title;
-    }
 }
